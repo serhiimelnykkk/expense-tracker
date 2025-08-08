1d7162643d54de9e028c23419a1bbb3841315545
smrlyman <smrlyman@gmail.com>
2025-08-08 10:19:49 +0300

    feat: Add charts and show test data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { expenses } from "./store";
+import {
+  PieChart,
+  BarChart,
+  Bar,
+  Pie,
+  Tooltip,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+} from "recharts";
+import { pieChartData, barChartData } from "./data";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <PieChart width={730} height={250}>
+        <Pie data={pieChartData} dataKey="price" label />
+        <Tooltip />
+      </PieChart>
+      <BarChart width={730} height={250} data={barChartData}>
+        <CartesianGrid />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Legend />
+        <Tooltip />
+        <Bar dataKey="price" />
+      </BarChart>
+      {expenses.map((expense) => (
+        <div key={expense.id}>
+          {expense.id} {expense.name} {expense.category}{" "}
+          {expense.date.toLocaleDateString()} {expense.price}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
